0fb6523c76404731e258e9462afa683ac573aaf4
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-24 07:35:29 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// src/server.js
-import app from "./app.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
