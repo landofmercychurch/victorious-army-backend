44eb06bdc9170d4c4f921c0d9509856766f3ce87
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-24 22:51:39 +0100

    Update likesRoutes.js
    Done

--- src/routes/likesRoutes.js
+++ src/routes/likesRoutes.js
@@ -4,7 +4,10 @@ import { createLike, countLikes } from "../controllers/likesController.js";
 
 const router = express.Router();
 
+// Like a post/sermon
 router.post("/", createLike);
+
+// Count likes for a post/sermon
 router.get("/count/:postId", countLikes);
 
 export default router;
