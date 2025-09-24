abd49192d85f2c69b636a0c9b9aadf76ba63a3f4
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-24 07:22:42 +0100

    Add files via upload

---
+++ src/config/supabase.js
@@ -0,0 +1,13 @@
+// src/config/supabase.js
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error("Missing Supabase server env vars");
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
