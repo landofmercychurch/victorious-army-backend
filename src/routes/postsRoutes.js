e1c49288ca7ee4c8a5f59d6ba3307ad1e8fac0ec
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-27 12:03:11 +0100

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { listPosts, getPostById } from "../controllers/postsController.js";
+import { listPosts } from "../controllers/postsController.js";
 
 const router = express.Router();
 
 // Public can view all posts
 router.get("/", listPosts);
-
-// Public can view single post (for OG meta / WhatsApp preview)
-router.get("/:id", getPostById);
 
 export default router;
