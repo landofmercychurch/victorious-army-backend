4c25fb3d230853bede20548f53923a89c05ed70b
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-27 08:40:24 +0100

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,25 +1,10 @@
-// src/routes/postsRoutes.js
+// src/routes/PostsRoutes.js
 import express from "express";
-import multer from "multer";
-import { listPosts, createPost, deletePost, deleteComment } from "../controllers/postsController.js";
-import { requireAdmin } from "../middleware/adminAuth.js";
+import { listPosts } from "../controllers/postsController.js";
 
 const router = express.Router();
-const upload = multer({ 
-  storage: multer.memoryStorage(), 
-  limits: { fileSize: 10 * 1024 * 1024 } 
-});
 
-// Admin can view all posts
-router.get("/", requireAdmin, listPosts);
-
-// Admin can create a post
-router.post("/", requireAdmin, upload.single("image"), createPost);
-
-// Admin can delete a post
-router.delete("/:id", requireAdmin, deletePost);
-
-// Admin can delete a post comment
-router.delete("/comments/:id", requireAdmin, deleteComment);
+// Public can view all posts
+router.get("/", listPosts);
 
 export default router;
