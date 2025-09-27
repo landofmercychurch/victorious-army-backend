8ce92f66622e88e9d7171b925c2f30db91a6c987
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-27 03:03:28 +0100

    Update commentsRoutes.js

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -1,17 +1,15 @@
 // src/routes/commentsRoutes.js
 import express from "express";
-import { listCommentsForPost, createComment, deleteComment } from "../controllers/commentsController.js";
-import { requireAdmin } from "../middleware/adminAuth.js";
+import { createComment, getComments } from "../controllers/commentsController.js";
 
 const router = express.Router();
 
-// GET all comments for a post
-router.get("/post/:postId", listCommentsForPost);
+// Guest can fetch comments for a post or sermon
+// Example: GET /api/comments/:id?type=post
+// Example: GET /api/comments/:id?type=sermon
+router.get("/:id", getComments);
 
 // Guest can create comment
 router.post("/", createComment);
-
-// Admin can delete comment
-router.delete("/:id", requireAdmin, deleteComment);
 
 export default router;
