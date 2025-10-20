030bd741fcca18ca275ccd74b9901dac237c96a5
landofmercychurch <oluwaseyio710@gmail.com>
2025-10-20 22:12:16 +0100

    Update app.js
    cors settup

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ const app = express();
 const allowedOrigins = [
   "https://victoriousarmyrevivalmovementchurch.netlify.app",
   "http://localhost:5173",
+  "http://127.0.0.1:5500",
   "http://localhost:5500"
 ];
 
