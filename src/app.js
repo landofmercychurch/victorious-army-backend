8dc4e3b23f6b72f890dd355d5cd88be376aea84c
landofmercychurch <oluwaseyio710@gmail.com>
2025-10-29 06:45:34 +0100

    Update app.js
    done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { handleFileUpload } from "../controllers/uploadsStatusController.js";
+import { handleFileUpload } from "./controllers/uploadsStatusController.js";
 
 dotenv.config();
 
