5dbba704cd1c6921f274bb11f147177128634fc0
landofmercychurch <oluwaseyio710@gmail.com>
2025-11-18 08:48:52 +0100

    Update adminCommentsRoutes.js

--- src/routes/adminCommentsRoutes.js
+++ src/routes/adminCommentsRoutes.js
@@ -1,15 +1,16 @@
 // src/routes/adminCommentsRoutes.js
 import express from "express";
-import { requireAdmin } from "../middleware/adminAuth.js";
+import { authenticateJWT } from "../middleware/adminAuth.js";
+
 import { listComments, deleteComment } from "../controllers/adminCommentsController.js";
 
 const router = express.Router();
 
 // Admin: list comments for a post or sermon
 // Example: GET /api/admin/comments/123?type=sermon
-router.get("/:id", requireAdmin, listComments);
+router.get("/:id", authenticateJWT, listComments);
 
 // Admin: delete comment
-router.delete("/:id", requireAdmin, deleteComment);
+router.delete("/:id", authenticateJWT, deleteComment);
 
 export default router;
